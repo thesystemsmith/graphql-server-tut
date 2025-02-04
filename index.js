0244acd125892ade4f9eec9cc946586a9c580217
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-02-04 15:06:40 +0530

    added apollo server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
     hello: String!
-    randomNubmer: Int!
+    randomNumber: Int!
   }
 `
 // the Query type outlines all the queries that can be called by the client
@@ -13,3 +13,26 @@ const typeDefs = gql`
 // The exclamation mark (!) tells Apollo Server that a result is required
 
 // Here, we define two queries, one returns a String and another returns a Int
+
+
+// When a query is called a resolver with the same name is run
+// The API returns whatever is returned by the resolver
+// We are using arrow functions so the "return" keyword is not required
+const resolvers = {
+    // The name of the resolver must match the name of the query in the typeDefs
+    Query: {
+      // When the hello query is invoked "Hello world" should be returned
+      hello: () => "Hello world!",
+      // When we call the randomNumber query, it should return a number between 0 and 10
+      randomNumber: () => Math.round(Math.random() * 10),
+    },
+  };
+
+//create apollo server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+// start the server at 8080
+server.listen({port: 8080}).then(({url}) => console.log(`graphql server running at ${url}`))
