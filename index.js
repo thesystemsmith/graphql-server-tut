166a20488e558dae4f4a28bdc7629276ea769925
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-02-04 15:15:51 +0530

    data retrieval

--- index.js
+++ index.js
@@ -3,36 +3,47 @@ const { ApolloServer, gql } = require("apollo-server");
 // typeDefs tell the GraphQL server what data to expect
 // Notice the gql tag, this converts your string into GraphQL strings that can be read by Apollo
 const typeDefs = gql`
+  type User {
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+
   type Query {
     hello: String!
     randomNumber: Int!
+    queryUsers: [User]!
   }
-`
+`;
 // the Query type outlines all the queries that can be called by the client
-// hello and randomNumber are the names of the queries
 // The exclamation mark (!) tells Apollo Server that a result is required
 
-// Here, we define two queries, one returns a String and another returns a Int
-
-
-// When a query is called a resolver with the same name is run
-// The API returns whatever is returned by the resolver
-// We are using arrow functions so the "return" keyword is not required
+// When a query is called a resolver with the same name is run, The API returns whatever is returned by the resolver
 const resolvers = {
-    // The name of the resolver must match the name of the query in the typeDefs
-    Query: {
-      // When the hello query is invoked "Hello world" should be returned
-      hello: () => "Hello world!",
-      // When we call the randomNumber query, it should return a number between 0 and 10
-      randomNumber: () => Math.round(Math.random() * 10),
-    },
-  };
+  // The name of the resolver must match the name of the query in the typeDefs
+  Query: {
+    hello: () => "Hello world!",
+    randomNumber: () => Math.round(Math.random() * 10),
+    queryUsers: () => users,
+  },
+};
+
+//datastore
+const users = [
+  {
+    firstName: "GraphQL",
+    lastName: "isCool",
+    email: "GraphQL@isCool.com",
+  },
+];
 
 //create apollo server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 // start the server at 8080
-server.listen({port: 8080}).then(({url}) => console.log(`graphql server running at ${url}`))
+server
+  .listen({ port: 8080 })
+  .then(({ url }) => console.log(`graphql server running at ${url}`));
